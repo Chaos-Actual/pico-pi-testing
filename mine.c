321aa05d464041aa7e1db7405be9a6fe8bce5356
McGuire1812 <mcguire.mark.d@gmail.com>
2022-08-10 23:42:53 -0400

    adding libraries

---
+++ mine.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "mine.h"
+void mytest(void){
+    printf("DID this WORK!?");
+
+};
--- test.c
+++ test.c
@@ -1,24 +1,51 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/gpio.h"
-#include "pico/binary_info.h"
-
-const uint LED_PIN = 25;
+#include "mine.h"
+#include "lcd.h"
 
 int main() {
-
-    bi_decl(bi_program_description("This is a test binary."));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
-
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#else
     stdio_init_all();
-
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (1) {
+    gpio_put(LED_PIN, 1);
+    sleep_ms(11950);
+     printf("Here1");
+
+    printf("Here2");
+    i2c_init(i2c_default,100*1000);
+    printf("Here3");
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    printf("Here4");
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    printf("Here5\n");
+    //  Make the I2C pins available to picotool
+   // bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN,GPIO_FUNC_I2C));
+
+    lcd_init();
+    printf("hell0 6");
+    char lcd_text[18] = "SUP BITCH!";
+    lcd_string(lcd_text);
+
+    while (true) {
+        mytest();
+
+        printf("hello this is the const %d \n",MAX_CHARS);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
-        sleep_ms(1000);
     }
+#endif
 }
+
